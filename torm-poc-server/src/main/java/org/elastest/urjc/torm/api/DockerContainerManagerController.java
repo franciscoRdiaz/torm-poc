38f15af832a2c15495da6978bbdd104faf747c80
EduJGURJC <ejg.urjc@gmail.com>
2017-04-20 17:06:25 +0200

    /testInfo controller

--- torm-poc-server/src/main/java/org/elastest/urjc/torm/api/DockerContainerManagerController.java
+++ torm-poc-server/src/main/java/org/elastest/urjc/torm/api/DockerContainerManagerController.java
@@ -1,5 +1,13 @@
 package org.elastest.urjc.torm.api;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
 import org.elastest.urjc.torm.api.data.DockerContainerInfo;
 import org.elastest.urjc.torm.service.DockerContainerManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,24 +23,49 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/containers")
 public class DockerContainerManagerController {
-	
+
 	@Autowired
 	private DockerContainerManagerService dockerContainerManagerService;
-	
+
 	@CrossOrigin(origins = "http://localhost:4200")
-	@RequestMapping (value = "/", method = RequestMethod.POST)
+	@RequestMapping(value = "/", method = RequestMethod.POST)
 	@ResponseStatus(HttpStatus.CREATED)
-	public DockerContainerInfo createDockerContainer(@RequestBody DockerContainerInfo dockerContainerInfo){
-		
-		return dockerContainerManagerService.createDockerContainer();		
+	public DockerContainerInfo createDockerContainer(@RequestBody DockerContainerInfo dockerContainerInfo) {
+
+		return dockerContainerManagerService.createDockerContainer();
 	}
-	
-	@RequestMapping (value = "/id", method = RequestMethod.PUT)
+
+	@RequestMapping(value = "/id", method = RequestMethod.PUT)
 	@ResponseStatus(HttpStatus.OK)
-	public DockerContainerInfo startDockerContainer(@PathVariable String id, @RequestBody DockerContainerInfo dockerContainerInfo){
-		//DockerContainerInfo dockerContainerInfo = new DockerContainerInfo();
-		
+	public DockerContainerInfo startDockerContainer(@PathVariable String id,
+			@RequestBody DockerContainerInfo dockerContainerInfo) {
+		// DockerContainerInfo dockerContainerInfo = new DockerContainerInfo();
+
 		return dockerContainerInfo;
+	}
+
+	@RequestMapping(value = "/testInfo", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public String getTestResults() {
+		String testResult, line;
+		testResult = "";
+		InputStream fis;
+		try {
+			fis = new FileInputStream("/var/torm/torm-test-01/testsuites.json");
+			InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+			BufferedReader br = new BufferedReader(isr);
+			try {
+				while ((line = br.readLine()) != null) {
+					testResult += line;
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return testResult;
 	}
 
 }
